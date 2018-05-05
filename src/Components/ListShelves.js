9734e1203b314513aec08ce62a65ef4e06c0117d
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-05-05 09:11:20 -0400

    feature: Improve styling

--- src/Components/ListShelves.js
+++ src/Components/ListShelves.js
@@ -10,14 +10,20 @@ class ListShelves extends Component {
     render() {
         const books = this.props.books;
 
-        return (
-        <div>
-            <ul>
-                <li><Shelf name="Currently Reading" books={books.filter((book) => (book.shelf === "currentlyReading"))}/></li>
-                <li><Shelf name="Want To Read" books={books.filter((book) => (book.shelf === "wantToRead"))}/></li>
-                <li><Shelf name="Read" books={books.filter((book) => (book.shelf === "read"))}/></li>
-            </ul>
-        </div>
+        return (<div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <Shelf name="Currently Reading"
+                               books={books.filter((book) => (book.shelf === "currentlyReading"))}/>
+                        <Shelf name="Want To Read" books={books.filter((book) => (book.shelf === "wantToRead"))}/>
+                        <Shelf name="Read" books={books.filter((book) => (book.shelf === "read"))}/>
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book';
 
 class Shelf extends Component {
     static propTypes = {
@@ -11,12 +12,19 @@ class Shelf extends Component {
         const {name, books} = this.props;
 
         return (
-            <div>
-                <h2>{name}</h2>
-                <ul>
-                    {books.map((book) => (<li>{book.title}</li>))}
-                </ul>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{name}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.map((book) => (
+                            <li key={book.id}>
+                                <Book book={book}/>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
             </div>
+
         )
     }
 }
