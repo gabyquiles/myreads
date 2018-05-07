0f375719acc9b4efb2a9d5c467840d9a063a6797
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-05-06 20:32:55 -0400

    feature: Add Search Functionality

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -8,15 +8,17 @@ class Book extends Component {
 
     render() {
         const book = this.props.book;
+        console.log(book.authors);
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover"
-                         style={{
-                             width: 128,
-                             height: 193,
-                             backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                         }}></div>
+                    {book.imageLinks.thumbnail ? (
+                        <div className="book-cover"
+                             style={{
+                                 width: 128,
+                                 height: 193,
+                                 backgroundImage: `url(${book.imageLinks.thumbnail})`
+                             }}></div>) : ''}
                     <div className="book-shelf-changer">
                         <select>
                             <option value="none" disabled>Move to...</option>
@@ -28,7 +30,7 @@ class Book extends Component {
                     </div>
                 </div>
                 <div className="book-title">{book.title}</div>
-                <div className="book-authors">Harper Lee</div>
+                <div className="book-authors">{book.authors}</div>
             </div>
         )
     }
--- src/Components/SearchBook.js
+++ src/Components/SearchBook.js
@@ -1,26 +1,55 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI';
+import Book from './Book';
 
 class SearchBook extends Component {
+    state = {
+        books: [],
+        hasError: false
+    };
+
+    searchBooks(query) {
+        this.setState(() => ({hasError: false}));
+        BooksAPI.search(query.trim()).then((books) => {
+            (books && 'error' in books) ? this.setState(() => ({hasError: true})) : this.setState(() => ({books}));
+            // books.error ? console.log("Hay books") : console.log("No hay nbooks");
+            // this.setState(() => ({hasError: false}));
+            // this.setState(() => (books.books ? {books} : {hasError: true}));
+        })
+    }
+
     render() {
+        const {books, hasError} = this.state;
+        console.log(hasError)
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
+                    <Link
+                        to="/"
+                        className="close-search"
+                    >
+                        Close
+                    </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author"/>
-
+                        <input type="text" placeholder="Search by title or author"
+                               onChange={(event) => this.searchBooks(event.target.value)}/>
                     </div>
                 </div>
-                <div className="search-books-results">
-                    <ol className="books-grid"></ol>
-                </div>
+                {!hasError ?
+                    <div className="search-books-results">
+                        <ol className="books-grid">
+                            {books && books.map((book) => (
+                                <li key={book.id}>
+                                    <Book book={book}/>
+                                </li>
+                            ))}
+                        </ol>
+                    </div> :
+                    <div>
+                        <h2>No books found</h2>
+                    </div>
+                }
             </div>
         )
     }
