6ff6569dfb86c28ce9356aca5ba9aef1bab76416
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-05-07 22:01:12 -0400

    feature: Change ListShelves to functional stateless component

--- src/Components/ListShelves.js
+++ src/Components/ListShelves.js
@@ -1,43 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Shelf from "./Shelf";
 
-class ListShelves extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        refreshBooks: PropTypes.func
-    };
+const ListShelves = ({books, changeShelf}) => {
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <Shelf name="Currently Reading"
+                           books={books.filter((book) => (book.shelf === "currentlyReading"))}
+                           changeShelf={changeShelf}/>
+                    <Shelf name="Want To Read" books={books.filter((book) => (book.shelf === "wantToRead"))}
+                           changeShelf={changeShelf}/>
+                    <Shelf name="Read" books={books.filter((book) => (book.shelf === "read"))}
+                           changeShelf={changeShelf}/>
 
-    render() {
-        const books = this.props.books;
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <Shelf name="Currently Reading"
-                               books={books.filter((book) => (book.shelf === "currentlyReading"))}
-                               changeShelf={this.props.changeShelf}/>
-                        <Shelf name="Want To Read" books={books.filter((book) => (book.shelf === "wantToRead"))}
-                               changeShelf={this.props.changeShelf}/>
-                        <Shelf name="Read" books={books.filter((book) => (book.shelf === "read"))}
-                               changeShelf={this.props.changeShelf}/>
-
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link
-                        to="/search"
-                    >
-                        Add a book
-                    </Link>
                 </div>
             </div>
-        )
-    }
-}
+            <div className="open-search">
+                <Link
+                    to="/search"
+                >
+                    Add a book
+                </Link>
+            </div>
+        </div>
+    )
+};
+
+ListShelves.PropTypes = {
+    books: PropTypes.array.isRequired,
+    refreshBooks: PropTypes.func
+};
 
 export default ListShelves;
